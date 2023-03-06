a8a0969babc47ebc896157b4260dc471bed29a24
sukhendra <mpct5jan1998@gmail.com>
2023-03-06 10:33:07 +0530

    completed

--- app.js
+++ app.js
@@ -5,11 +5,35 @@ const container = document.querySelectorAll(".slide");
 
 let index = 0;
 
-nextBtn.addEventListener("click", () => {
-  console.log(container[0]);
-  container[index].classList.add("display");
+const display = (index) => {
+  container.forEach((slide) => {
+    console.log(slide);
+    slide.style.display = "none";
+  });
+  container[index].style.display = "flex";
+}
+
+
+
+const nextSlide = () => {
   index++;
-  if (index === 3) {
-    index = 0;
+  if(index > container.length-1) {
+    index=0;
   }
-});
+  console.log(index);
+  display(index);
+}
+
+const prevSlide = () => {
+  index--;
+  if(index < 0) {
+    index = container.length-1;
+  }
+  console.log(index);
+  display(index);
+}
+
+display(index);
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
